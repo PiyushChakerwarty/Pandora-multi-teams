13801a20c63cc4391c8899b29b84dcbee8af5986
PiyushChakerwarty <piyushchakerwarty97@gmail.com>
2023-10-04 18:16:50 +0530

    Added subtraction feature

---
+++ subtraction.py
@@ -0,0 +1,5 @@
+# program to subtract two numbers taken as user input
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+print("Subtraction of numbers is", num1 - num2)
